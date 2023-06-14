08c40e245ae4700225ce1dc4c597d5a88fd19d01
Kaveesha <kliyanagunawardhana@myseneca.ca>
2023-06-13 22:27:03 -0400

    Handle the error

--- Lab1a.py
+++ Lab1a.py
@@ -4,3 +4,15 @@ def helloWorld():
 
 helloWorld()
 
+import datetime
+
+def calculate_age():
+    try:
+        current_year = datetime.datetime.now().year
+        birth_year = int(input("Enter your birth year: "))
+        age = current_year - birth_year
+        return age
+    except TypeError:
+        print("Please enter an integer for the birth year.")
+        return None
+
